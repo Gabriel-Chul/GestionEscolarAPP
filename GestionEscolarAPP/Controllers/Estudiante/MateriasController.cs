6d48c145b8bf966caa6a2c1c770199a76b323ebe
Gabriel Chul <alfredo-chul@hotmail.com>
2024-10-18 21:34:13 -0600

    Añadido la vista, el controlador y el modelo para Materias del lado del estudiante

---
+++ GestionEscolarAPP/Controllers/Estudiante/MateriasController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using GestionEscolarAPP.Models.Estudiante;
+using System.Collections.Generic;
+
+namespace GestionEscolarAPP.Controllers.Estudiante
+{
+    [Route("Estudiante/[action]")]
+    public class MateriasController : Controller
+    {
+        [HttpGet]
+        public IActionResult Materias()
+        {
+            var model = new List<MateriasModel>
+            {
+                new() { Materia = "Matemáticas", Dia = "Lunes y Miércoles", Hora = "10:00 - 11:30", Aula = "Aula 101", Maestro = "Prof. Juan Pérez" },
+                new() { Materia = "Lenguaje", Dia = "Martes y Jueves", Hora = "09:00 - 10:30", Aula = "Aula 102", Maestro = "Prof. María López" },
+                new() { Materia = "Ciencias", Dia = "Lunes y Miércoles", Hora = "12:00 - 13:30", Aula = "Aula 201", Maestro = "Prof. Carlos Fernández" },
+                new() { Materia = "Química", Dia = "Martes y Jueves", Hora = "11:00 - 12:30", Aula = "Aula 202", Maestro = "Prof. Ana Martínez" },
+                new() { Materia = "Sociales", Dia = "Viernes", Hora = "09:00 - 11:00", Aula = "Aula 203", Maestro = "Prof. Luis Gómez" }
+            };
+
+            // Especifica la ruta completa de la vista
+            return View("~/Views/Estudiante/Materias.cshtml", model);
+        }
+    }
+}
---
+++ GestionEscolarAPP/Models/Estudiante/MateriasModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GestionEscolarAPP.Models.Estudiante
+{
+    public class MateriasModel
+    {
+        [Required]
+        public required string Materia { get; set; }
+
+        [Required]
+        public required string Dia { get; set; }
+
+        [Required]
+        public required string Hora { get; set; }
+
+        [Required]
+        public required string Aula { get; set; }
+
+        [Required]
+        public required string Maestro { get; set; }
+    }
+}
